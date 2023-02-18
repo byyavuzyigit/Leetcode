7a5c192e05114bd1a69e1d41daf1d979b36c8796
byyavuzyigit <byavuzyigit@etu.edu.tr>
2023-02-18 14:36:51 +0300

    problem solved

--- Problem104.java
+++ Problem104.java
@@ -1,19 +1,19 @@
 public class Problem104 {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
+
+     // Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+          }
+      }
+
     public int maxDepth(TreeNode root) {
         //base case
         if(root == null)
